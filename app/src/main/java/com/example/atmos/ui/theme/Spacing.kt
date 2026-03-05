56c234730b426c8c37395a6ad40a873bc5683318
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-05 02:51:12 +0200

    Add fonts.

---
+++ app/src/main/java/com/example/atmos/ui/theme/Spacing.kt
@@ -0,0 +1,14 @@
+package com.example.atmos.ui.theme
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val XXSmall  = 2.dp
+    val XSmall   = 4.dp
+    val Small    = 8.dp
+    val Medium   = 12.dp
+    val Large    = 16.dp
+    val XLarge   = 24.dp
+    val XXLarge  = 32.dp
+    val XXXLarge = 48.dp
+    val Huge     = 64.dp
+}
