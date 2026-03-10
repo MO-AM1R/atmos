fe52b73617490000828f6e2fb5ed5fb29a57634a
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-10 08:43:22 +0200

    Apply Dependency Injection to App database.

---
+++ app/src/main/java/com/example/atmos/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.example.atmos.di
+import android.app.Application
+import androidx.room.Room
+import com.example.atmos.data.database.AppDatabase
+import com.example.atmos.data.database.dao.WeatherDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(application: Application): AppDatabase{
+        return Room
+            .databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                "weather.db"
+            ).build()
+    }
+
+    @Provides
+    fun provideWeatherDao(appDatabase: AppDatabase): WeatherDao{
+        return appDatabase.weatherDao()
+    }
+}
