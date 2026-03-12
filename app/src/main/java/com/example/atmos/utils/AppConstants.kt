e349e72e06da2297fb19ff7575f0d0905d4b0a6c
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-12 06:30:45 +0200

    Add icons end point.

--- app/src/main/java/com/example/atmos/utils/AppConstants.kt
+++ app/src/main/java/com/example/atmos/utils/AppConstants.kt
@@ -4,6 +4,7 @@ import com.example.atmos.BuildConfig
 
 object AppConstants {
     const val BASE_URL = "https://api.openweathermap.org/"
+    const val ICONS_BASE_URL = "https://openweathermap.org/payload/api/media/file/"
     const val WEATHER_BASE_URL = "data/2.5/"
     const val GEO_BASE_URL = "geo/1.0/"
 
