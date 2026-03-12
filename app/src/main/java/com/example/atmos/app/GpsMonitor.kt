e3b7646d6a2631cad248efd358c9ad28a87941b1
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-12 06:28:13 +0200

    Add Monitor for GPS and Network States.

---
+++ app/src/main/java/com/example/atmos/app/GpsMonitor.kt
@@ -0,0 +1,56 @@
+package com.example.atmos.app
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+class GpsMonitor @Inject constructor(
+    @param:ApplicationContext private val context: Context
+) {
+
+    val gpsStatus: Flow<Boolean> = callbackFlow {
+
+        trySend(isLocationEnabled())
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                if (intent.action == LocationManager.PROVIDERS_CHANGED_ACTION) {
+                    trySend(isLocationEnabled())
+                }
+            }
+        }
+
+        val filter = IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION)
+
+        ContextCompat.registerReceiver(
+            context,
+            receiver,
+            filter,
+            ContextCompat.RECEIVER_NOT_EXPORTED
+        )
+
+        awaitClose {
+            context.unregisterReceiver(receiver)
+        }
+
+    }.distinctUntilChanged()
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+}
