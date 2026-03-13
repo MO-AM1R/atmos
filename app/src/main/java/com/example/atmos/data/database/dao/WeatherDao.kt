53bd7c7e5c7ab9405b8c0d1bd16cdda49d95eb4e
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-13 06:46:34 +0200

    Implement WeatherDao.

--- app/src/main/java/com/example/atmos/data/database/dao/WeatherDao.kt
+++ app/src/main/java/com/example/atmos/data/database/dao/WeatherDao.kt
@@ -1,7 +1,26 @@
 package com.example.atmos.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.atmos.data.database.entity.CurrentWeatherEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun cacheCurrentWeather(currentWeatherEntity: CurrentWeatherEntity)
+
+    @Query("DELETE FROM CURRENT_WEATHER")
+    suspend fun clearCachedWeather()
+
+    @Query("SELECT cachedAtUnix FROM CURRENT_WEATHER LIMIT 1")
+    fun getCachedTime(): Flow<Long?>
+
+    @Query("SELECT * FROM CURRENT_WEATHER LIMIT 1")
+    fun getCachedWeather(): Flow<CurrentWeatherEntity?>
+
+    @Query("SELECT COUNT(*) FROM CURRENT_WEATHER WHERE cachedAtUnix > :expiryTime")
+    fun isCacheValid(expiryTime: Long): Flow<Boolean>
 }
