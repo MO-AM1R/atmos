fd76716900de1bc8dddad1dd72a1087893854bb9
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-13 06:47:00 +0200

    Implement Weather Local Data Source classes.

--- app/src/main/java/com/example/atmos/data/datasource/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/example/atmos/data/datasource/local/WeatherLocalDataSource.kt
@@ -1,4 +1,19 @@
 package com.example.atmos.data.datasource.local
 
+import com.example.atmos.data.database.entity.CurrentWeatherEntity
+import com.example.atmos.data.database.entity.HourlyForecastEntity
+import kotlinx.coroutines.flow.Flow
+
+
 interface WeatherLocalDataSource {
+    suspend fun cacheCurrentWeather(currentWeatherEntity: CurrentWeatherEntity)
+    suspend fun cacheForecast(hourlyForecasts: List<HourlyForecastEntity>)
+
+    fun getCachedWeather(): Flow<CurrentWeatherEntity?>
+    fun getCachedForecasts(): Flow<List<HourlyForecastEntity>>
+
+    fun getCachedTime(): Flow<Long?>
+    fun isCacheValid(): Flow<Boolean>
+
+    suspend fun clearAllCache()
 }
