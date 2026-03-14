bf526e2ff9685f09bb021cc180c9cf1a07590055
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-14 22:35:50 +0200

    Add get current location helper method.

--- app/src/main/java/com/example/atmos/utils/LocationHelper.kt
+++ app/src/main/java/com/example/atmos/utils/LocationHelper.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.Priority
-
+import android.content.Context
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.mapbox.geojson.Point
 
 @SuppressLint("MissingPermission")
 fun requestLocation(
@@ -16,5 +19,26 @@ fun requestLocation(
         null
     ).addOnSuccessListener {
         it?.let(onLocationReady)
+    }
+}
+
+
+@SuppressLint("MissingPermission")
+fun getCurrentLocation(
+    context : Context,
+    onResult: (Point?) -> Unit
+) {
+    val client = LocationServices.getFusedLocationProviderClient(context)
+    client.getCurrentLocation(
+        Priority.PRIORITY_HIGH_ACCURACY,
+        CancellationTokenSource().token
+    ).addOnSuccessListener { location ->
+        if (location != null) {
+            onResult(Point.fromLngLat(location.longitude, location.latitude))
+        } else {
+            onResult(null)
+        }
+    }.addOnFailureListener {
+        onResult(null)
     }
 }
