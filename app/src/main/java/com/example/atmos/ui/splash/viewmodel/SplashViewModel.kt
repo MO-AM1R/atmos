b3bcb3c89a0c39c7f237dcdd28162a91fa9f6251
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-15 01:28:24 +0200

    Refactor see onboarding process in (Splash, Onboarding) Screens.

--- app/src/main/java/com/example/atmos/ui/splash/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/atmos/ui/splash/viewmodel/SplashViewModel.kt
@@ -1,23 +1,29 @@
 package com.example.atmos.ui.splash.viewmodel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.atmos.domain.repository.UserPreferencesRepository
 import com.example.atmos.ui.splash.state.SplashUIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val userPreferencesRepository: UserPreferencesRepository
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow(
-        SplashUIState(
-            onboardingSeenBefore =
-                userPreferencesRepository.isOnboardingSeenBefore()
-        )
-    )
-
+    private val _state = MutableStateFlow(SplashUIState())
     val state = _state.asStateFlow()
+
+
+    init {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(onboardingSeenBefore = userPreferencesRepository.hasSeenOnboarding())
+            }
+        }
+    }
 }
