b43eb0d902816a3cfe33174be953070f29e7fc00
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-03-15 01:28:47 +0200

    Refactor Settings State (include selected point).

--- app/src/main/java/com/example/atmos/ui/settings/state/SettingsEvent.kt
+++ app/src/main/java/com/example/atmos/ui/settings/state/SettingsEvent.kt
@@ -4,11 +4,13 @@ import com.example.atmos.data.enums.Language
 import com.example.atmos.data.enums.LocationOption
 import com.example.atmos.data.enums.TemperatureUnit
 import com.example.atmos.data.enums.WindUnit
+import com.example.atmos.domain.model.StoredPoint
 
 sealed class SettingsEvent {
-    data class OnGpsToggled(val locationOption: LocationOption): SettingsEvent()
+    data class OnLocationPointSelected(val storedPoint: StoredPoint): SettingsEvent()
     data class OnTemperatureUnitSelected(val newOption: TemperatureUnit): SettingsEvent()
     data class OnWindUnitSelected(val newOption: WindUnit): SettingsEvent()
-    data class OnLanguageClicked(val newOption: Language): SettingsEvent()
+    data class OnLanguageSelected(val newOption: Language): SettingsEvent()
+    data class OnLocationOptionChanged(val newOption: LocationOption): SettingsEvent()
     object OnNavigateToMapScreen: SettingsEvent()
 }
--- app/src/main/java/com/example/atmos/ui/settings/state/SettingsUiState.kt
+++ app/src/main/java/com/example/atmos/ui/settings/state/SettingsUiState.kt
@@ -4,12 +4,16 @@ import com.example.atmos.data.enums.Language
 import com.example.atmos.data.enums.LocationOption
 import com.example.atmos.data.enums.TemperatureUnit
 import com.example.atmos.data.enums.WindUnit
+import com.example.atmos.domain.model.StoredPoint
 
 
 data class SettingsUiState(
     val locationOption: LocationOption = LocationOption.GPS,
+    val storedPoint: StoredPoint? = null,
+    val storedLocationName: String? = null,
+    val isLoadingLocationName: Boolean = false,
     val temperatureUnit: TemperatureUnit = TemperatureUnit.CELSIUS,
     val windUnit: WindUnit = WindUnit.METERS_PER_SECOND,
-    val storedLocation: String? = null,
-    val language: Language = Language.ENGLISH
+    val language: Language = Language.ENGLISH,
+    val isRefreshing: Boolean = false
 )
