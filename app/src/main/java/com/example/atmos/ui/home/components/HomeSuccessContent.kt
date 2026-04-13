f1dfc97bc5f60ae451ec89e32f8bb828f6228338
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-04-13 10:09:55 +0200

    Extract the location header to external component.

--- app/src/main/java/com/example/atmos/ui/home/components/HomeSuccessContent.kt
+++ app/src/main/java/com/example/atmos/ui/home/components/HomeSuccessContent.kt
@@ -1,38 +1,24 @@
 package com.example.atmos.ui.home.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.atmos.R
 import com.example.atmos.domain.model.UserPreferences
-import com.example.atmos.ui.core.components.ResourceIcon
 import com.example.atmos.ui.home.state.HomeUiState
 import com.example.atmos.ui.theme.Spacing
-import com.example.atmos.ui.theme.WeatherTypography
-import com.example.atmos.ui.theme.White
-import com.example.atmos.ui.theme.extraColors
-import com.example.atmos.utils.toLocalizedCountryNameWithFlag
 import io.github.fletchmckee.liquid.LiquidState
 
 @Composable
 fun HomeSuccessContent(
     modifier: Modifier = Modifier,
     uiState: HomeUiState = HomeUiState(),
-    onRefresh: () -> Unit = {},
     userPreferencesState: UserPreferences? = null,
     liquidState: LiquidState,
 ) {
@@ -48,38 +34,7 @@ fun HomeSuccessContent(
 
         Spacer(modifier = Modifier.height(60.dp))
 
-        //TODO: extract to separated file
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                ResourceIcon(
-                    modifier = Modifier.size(18.dp),
-                    resourceId = R.drawable.ic_location_point,
-                    color = White,
-                )
-
-                Text(
-                    text = uiState.currentWeather?.cityName ?: "--",
-                    style = WeatherTypography.headlineMedium,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.extraColors.textPrimary
-                )
-            }
-
-            Text(
-                text = uiState.forecastDays?.cityName?.plus(", ${uiState.forecastDays.countryCode.toLocalizedCountryNameWithFlag()}")
-                    ?: "--",
-                style = WeatherTypography.headlineMedium,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.extraColors.textPrimary
-            )
-        }
+        LocationHeader(uiState)
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -87,7 +42,6 @@ fun HomeSuccessContent(
             modifier = Modifier,
             weather = uiState.currentWeather,
             temperatureUnit = userPreferencesState?.temperatureUnitOption,
-            onRefreshClick = onRefresh,
             liquidState = liquidState,
         )
 
---
+++ app/src/main/java/com/example/atmos/ui/home/components/LocationHeader.kt
@@ -0,0 +1,77 @@
+package com.example.atmos.ui.home.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.atmos.R
+import com.example.atmos.ui.core.components.ResourceIcon
+import com.example.atmos.ui.home.state.HomeUiState
+import com.example.atmos.ui.theme.WeatherTypography
+import com.example.atmos.ui.theme.White
+import com.example.atmos.ui.theme.extraColors
+import com.example.atmos.utils.toFlagEmoji
+import com.example.atmos.utils.toLocalizedCountryName
+
+@Composable
+fun LocationHeader(uiState: HomeUiState) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                ResourceIcon(
+                    modifier = Modifier.size(18.dp),
+                    resourceId = R.drawable.ic_location_point,
+                    color = White,
+                )
+
+                Text(
+                    text = uiState.currentWeather?.cityName ?: "--",
+                    style = WeatherTypography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.extraColors.textPrimary
+                )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = uiState.forecastDays?.countryCode?.toFlagEmoji() ?: "--",
+                    style = WeatherTypography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.extraColors.textPrimary
+                )
+
+                Text(
+                    text = uiState.forecastDays?.cityName
+                        ?.plus(", ${uiState.forecastDays.countryCode.toLocalizedCountryName()}")
+                        ?: "--",
+                    style = WeatherTypography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.extraColors.textPrimary
+                )
+            }
+        }
+    }
+}
